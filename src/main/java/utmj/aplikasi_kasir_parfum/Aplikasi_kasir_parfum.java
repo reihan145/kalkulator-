d2f7c011ccf00b431b9a663526f545a776c11457
HP <HP@LAPTOP-1Q3ULF3G>
2026-01-29 13:45:11 +0700

    kasir_parfum app intial commit

---
+++ src/main/java/utmj/aplikasi_kasir_parfum/Aplikasi_kasir_parfum.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package utmj.aplikasi_kasir_parfum;
+  import perhitungan_barang.from_utama;
+/**
+ *
+ * @author HP
+ */
+public class Aplikasi_kasir_parfum {
+
+    public static void main(String[] args) {
+       new from_utama().setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
